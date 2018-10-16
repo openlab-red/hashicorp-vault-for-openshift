7dd61137e61070c2908626f723e1d27e8fd551c1
Mattia Mascia <mmascia@redhat.com>
2018-10-16 12:06:01 +0200

    removed post

--- examples/spring-example/src/main/java/com/vault/VaultDemoApplication.java
+++ examples/spring-example/src/main/java/com/vault/VaultDemoApplication.java
@@ -11,20 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VaultDemoApplication {
-	
-	private Log log = LogFactory.getLog(VaultDemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaultDemoApplication.class, args);
 	}
-
-
-    @Value("${password}")
-    String password;
-    
-    @PostConstruct
-    private void postConstruct() {
-    	log.info("My password is: " + password);
-    }
     
 }
