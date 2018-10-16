4bcb3a4177d01e8484151195088610c1ac7eb594
Mattia Mascia <mmascia@redhat.com>
2018-10-16 11:54:17 +0200

    Refresh scope

--- examples/spring-example/src/main/java/com/vault/SecretController.java
+++ examples/spring-example/src/main/java/com/vault/SecretController.java
@@ -3,15 +3,17 @@ package com.vault;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class SecretController {
 	
 	private Log log = LogFactory.getLog(SecretController.class);
 
-	@Value("${password}")
+	@Value("${password:undefined}")
 	String password;
 
 	@RequestMapping("/secret")
