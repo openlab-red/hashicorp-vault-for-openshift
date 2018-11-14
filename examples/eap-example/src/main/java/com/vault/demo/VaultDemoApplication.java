f098c0edb720356c2a1e22b270b4d857b6eea4e4
Mattia Mascia <mmascia@redhat.com>
2018-11-14 20:52:11 +0100

    clean up

--- examples/eap-example/src/main/java/com/vault/demo/VaultDemoApplication.java
+++ examples/eap-example/src/main/java/com/vault/demo/VaultDemoApplication.java
@@ -1,13 +1,7 @@
 package com.vault.demo;
 
-import javax.annotation.PostConstruct;
-import javax.naming.InitialContext;
-import javax.naming.NameClassPair;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.net.URL;
 
 @ApplicationPath("/")
 public class VaultDemoApplication extends Application {
