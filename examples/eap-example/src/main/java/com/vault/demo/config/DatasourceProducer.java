484e9a8b4baab543999fc6959d2111052a66088c
Mattia Mascia <mmascia@redhat.com>
2018-11-16 11:31:57 +0100

    using datasource definition

--- examples/eap-example/src/main/java/com/vault/demo/config/DatasourceProducer.java
+++ examples/eap-example/src/main/java/com/vault/demo/config/DatasourceProducer.java
@@ -2,22 +2,24 @@ package com.vault.demo.config;
 
 import javax.annotation.Resource;
 import javax.annotation.sql.DataSourceDefinition;
+import javax.ejb.Startup;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
 import javax.sql.DataSource;
 
 
+
 @DataSourceDefinition(
         name = "java:jboss/datasources/SampleDS",
         className = "org.postgresql.xa.PGXADataSource",
-        url = "jdbc:postgresql://postgresql:5432/sampledb",
         user = "postgres",
         password = "postgres",
         serverName = "postgresql",
         portNumber = 5432,
         databaseName = "sampledb")
 @Singleton
-public class DataSourceProducer {
+@Startup
+public class DatasourceProducer {
 
     @Resource(lookup = "java:jboss/datasources/SampleDS")
     DataSource ds;
