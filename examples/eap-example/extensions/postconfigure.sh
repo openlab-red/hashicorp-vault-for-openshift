d91a3b41bbc4f885b58439e2340df1dc5005bbf2
Mattia Mascia <mmascia@redhat.com>
2018-11-16 10:32:26 +0100

    using ds

--- examples/eap-example/extensions/postconfigure.sh
+++ examples/eap-example/extensions/postconfigure.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/extensions/vault.cli
-${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/extensions/postgresql.cli
+
+# Using dynamic -ds.xml
+# ${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/extensions/postgresql.cli
