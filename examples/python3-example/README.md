4bf92e0902d303140fec6eeeb4344e6ddfc415c4
Ricardo Lourenco <rlourenc@redhat.com>
2020-03-24 08:29:08 +0100

    add yaml files, raw diagram, pytest

---
+++ examples/python3-example/README.md
@@ -0,0 +1,23 @@
+# Python Example
+
+## Build the Application
+
+```
+oc new-project pyvault
+
+oc new-app python:3.6~https://github.com/rflorenc/hashicorp-vault-for-openshift#py-example --context-dir=examples/python3-example/
+```
+
+## Deploy
+ 
+### Manual Sidecar Container
+
+```
+    oc apply -f examples/python3-example/python3-example.yaml
+```
+
+### Mutating Webhook Configuration
+
+```
+    oc apply -f examples/python3-example/python3-inject.yaml
+```
--- examples/python3-example/main.py
+++ examples/python3-example/main.py
@@ -1,23 +1,56 @@
+# -*- coding: utf-8 -*-
+""" Example python demo app """
+
 import logging
 import os
 import json
 from flask import Flask
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+LOG = logging.getLogger('werkzeug')
+LOG.setLevel(logging.ERROR)
 
-app_root = os.path.dirname(os.path.abspath(__file__))
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-flask_app = Flask(__name__)
+FLASK_APP = Flask(__name__)
 
-@flask_app.route('/secret', methods=['GET'])
-def get_secret():
-    with open(os.path.join(app_root, "resources/application.txt")) as f:
-        return(f.read())
+@FLASK_APP.route('/secret', methods=['GET'])
+def get_secret(secret_file_path='resources/application.txt'):
+    """ prints secret data as read from specified file """
 
-@flask_app.route('/healthz', methods=['GET'])
+    if not os.path.exists(os.path.join(APP_ROOT, secret_file_path)):
+        return json.dumps({'File not found': False}), 404
+
+    with open(os.path.join(APP_ROOT, secret_file_path)) as f:
+        return f.read()
+
+@FLASK_APP.route('/healthz', methods=['GET'])
 def check_healthz():
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    """ basic kubernetes healthcheck """
+
+    return json.dumps({'success':True}), 200
+
+@FLASK_APP.route('/env', methods=['GET'])
+def get_environment_variables():
+    """ dumps a json dict built from python3 environment variables """
+
+    config_location = os.environ.get('PYTHON3_CONFIG_LOCATION')
+    additional_config_location = os.environ.get('PYTHON3_CONFIG_ADDITIONAL_LOCATION')
+
+    return(json.dumps({
+        'PYTHON3_CONFIG_LOCATION': config_location,
+        'PYTHON3_CONFIG_ADDITIONAL_LOCATION': additional_config_location
+        }))
+
+@FLASK_APP.route('/', methods=['GET'])
+def get_api():
+    """ returns available api resources """
+
+    return json.dumps({
+        '/secret': 'reveals the stored secret',
+        '/healthz': 'healthcheck endpoint',
+        '/env': 'debug environment variables'
+    })
+
 
 if __name__ == "__main__":
-    flask_app.run(host='0.0.0.0', port='8080')
+    FLASK_APP.run(host='0.0.0.0', port='8080')
