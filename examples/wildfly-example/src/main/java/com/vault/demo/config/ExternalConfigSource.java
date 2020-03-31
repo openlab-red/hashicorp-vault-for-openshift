110a6caf752e82408de807bf2c9798b9c3f368cc
Mattia Mascia <mmascia@redhat.com>
2020-03-31 22:45:36 +0100

    nio

--- examples/wildfly-example/src/main/java/com/vault/demo/config/ExternalConfigSource.java
+++ examples/wildfly-example/src/main/java/com/vault/demo/config/ExternalConfigSource.java
@@ -1,39 +1,54 @@
 package com.vault.demo.config;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
-import org.eclipse.microprofile.config.Config;
+
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.spi.ConfigSource;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
 
 public class ExternalConfigSource implements ConfigSource {
 
     private final String CONFIG_PROPERTY_PATH = "com.vault.demo.config.path";
     private final String CONFIG_SOURCE_NAME = "ExternalConfigSource";
     private final int ORDINAL = 300;
+    final private String location;
+    final private Properties properties;
 
-    private String configSource;
+    
+    public ExternalConfigSource() throws IOException {
+        this.location = ConfigProvider.getConfig().getValue(CONFIG_PROPERTY_PATH, String.class);
+        this.properties = new Properties();
+
+        final Path path = FileSystems.getDefault().getPath(this.location);
+        this.properties.load(Files.newInputStream(path));
+	}
+
+
+    @Override
+    public String getValue(String s) {
+        return properties.getProperty(s);
+    }
 
     @Override
     public Map<String, String> getProperties() {
-
         final Map<String, String> map = new HashMap<>();
-        final Properties load = load();
 
-        load.stringPropertyNames()
+        properties.stringPropertyNames()
                 .stream()
-                .forEach(key-> map.put(key, load.getProperty(key)));
-
+                .forEach(key-> map.put(key, properties.getProperty(key)));
         return map;
     }
 
     @Override
     public Set<String> getPropertyNames() {
-        return load().stringPropertyNames();
+        return properties.stringPropertyNames();
     }
 
     @Override
@@ -41,32 +56,10 @@ public class ExternalConfigSource implements ConfigSource {
         return ORDINAL;
     }
 
-    @Override
-    public String getValue(String s) {
-        return load().getProperty(s);
-    }
 
     @Override
     public String getName() {
         return CONFIG_SOURCE_NAME;
     }
 
-
-    public String read(){
-
-        if(Objects.isNull(configSource)) {
-            this.configSource = ConfigProvider.getConfig().getValue(CONFIG_PROPERTY_PATH, String.class);
-        }
-        return this.configSource;
-    }
-
-    public Properties load() {
-        Properties properties = new Properties();
-        try(InputStream in = new FileInputStream(read())){
-            properties.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return properties;
-    }
 }
